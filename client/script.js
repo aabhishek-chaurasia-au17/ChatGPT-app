b7d1ead2984b839a43c241c15d133810ced69069
Abhishek <abhishekc838@gmail.com>
2023-05-04 22:05:39 +0530

    backend done

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = document.querySelector('from')
+const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 let loadInterval;
@@ -15,6 +15,82 @@ function loader(element) {
         if(element.textContent === '....'){
             element.textContent = '';
         }
-        
+
     }, 300)
 }
+
+
+function typeText(element, text) {
+    let index = 0;
+
+    let interval = setInterval(() => {
+        if(index < text.length){
+            element.innerHTML += text.chartAt[index];
+            index++;
+        }else{
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+function generateUniqueId(){
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16)
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `<div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+            <div class="profile">
+                <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
+            </div>
+            <div class="message" id=${uniqueId}>
+            ${value}</div>
+        </div>
+        </div>`
+    )
+}
+
+const handleSubmit = async(e) =>{
+    e.preventDefault();
+
+    const data = new FormData(form)
+
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    form.reset();
+
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId)
+
+    loader(messageDiv)
+
+    //fetch data from server -> bot's response
+
+    const response = await fetch('http://localhost:5000', {
+    mathod: 'POST',
+    headers:{
+        'Content-Type': 'appliation/json'
+    },
+    body: JSON.stringify({
+        prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval)
+} 
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13){
+        handleSubmit(e);
+    }
+})
